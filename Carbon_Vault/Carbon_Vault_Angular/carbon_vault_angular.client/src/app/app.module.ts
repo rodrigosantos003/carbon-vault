1ce72e92e134ccc2f5876bf78485e6134c220416
andre-castanho00 <201902108@estudantes.ips.pt>
2025-01-16 15:46:35 +0000

    Frontend

--- Carbon_Vault/Carbon_Vault_Angular/carbon_vault_angular.client/src/app/app.module.ts
+++ Carbon_Vault/Carbon_Vault_Angular/carbon_vault_angular.client/src/app/app.module.ts
@@ -3,15 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { HomeComponent } from './home/home.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent // Do not include standalone components here
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
