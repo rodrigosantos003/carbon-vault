e714592ec6f98c72661fe8527b497ea64bad19b4
andre-castanho00 <201902108@estudantes.ips.pt>
2025-01-16 22:41:57 +0000

    Adicionei o HomeComponent ao routing

--- Carbon_Vault/Carbon_Vault_Angular/carbon_vault_angular.client/src/app/app-routing.module.ts
+++ Carbon_Vault/Carbon_Vault_Angular/carbon_vault_angular.client/src/app/app-routing.module.ts
@@ -6,10 +6,11 @@ import { RegisterComponent } from './register/register.component';
 import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { RecoverPasswordComponent } from './recover-password/recover-password.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   { path: 'confirm-account', component: ConfirmAccountComponent },
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'forgot-password', component: ForgotPasswordComponent },
