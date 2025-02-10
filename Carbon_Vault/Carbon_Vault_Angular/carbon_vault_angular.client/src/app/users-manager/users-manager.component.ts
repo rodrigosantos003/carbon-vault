9affb729f0b665404d09b9c842c082cb63e6dd0d
andre-castanho00 <201902108@estudantes.ips.pt>
2025-02-10 19:24:58 +0000

    Delete Popup em eliminar utilizadores na pagina de Admin Gestão de Utilizadores

--- Carbon_Vault/Carbon_Vault_Angular/carbon_vault_angular.client/src/app/users-manager/users-manager.component.ts
+++ Carbon_Vault/Carbon_Vault_Angular/carbon_vault_angular.client/src/app/users-manager/users-manager.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { ConfirmAccountComponent } from '../confirm-account/confirm-account.component';
 
 @Component({
   selector: 'app-users-manager',
@@ -17,6 +18,30 @@ export class UsersManagerComponent {
   ngOnInit(): void {
     this.getAccounts();
     console.log(this.accounts);
+  }
+
+  openPopup() {
+    const overlay = document.getElementById('modalOverlay');
+    const delPopup = document.getElementById('delete');
+
+    if (overlay && delPopup) {
+      overlay.style.display = 'flex';
+      delPopup.style.display = 'block';
+    }
+  }
+
+  closePopup() {
+    const overlay = document.getElementById('modalOverlay');
+    const delPopup = document.getElementById('delete');
+
+    if (overlay && delPopup) {
+      overlay.style.display = 'none';
+      delPopup.style.display = 'none';
+    }
+  }
+
+  viewAccount(account: any) {
+    console.log(account.id);
   }
 
   getAccounts(): void {
