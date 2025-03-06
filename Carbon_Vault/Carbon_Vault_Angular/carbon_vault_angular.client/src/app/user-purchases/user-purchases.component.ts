22494ba8d16c6f04309bdde87866047a1781c356
Rodrigo Santos <rodrigo_santos003@outlook.com>
2025-03-06 19:38:03 +0000

    Populate purchases with endpoint data

--- Carbon_Vault/Carbon_Vault_Angular/carbon_vault_angular.client/src/app/user-purchases/user-purchases.component.ts
+++ Carbon_Vault/Carbon_Vault_Angular/carbon_vault_angular.client/src/app/user-purchases/user-purchases.component.ts
@@ -1,12 +1,53 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { AuthService } from '../auth-service.service';
+import { AlertsService } from '../alerts.service';
 
 @Component({
   selector: 'app-user-purchases',
   standalone: false,
-  
+
   templateUrl: './user-purchases.component.html',
   styleUrl: './user-purchases.component.css'
 })
 export class UserPurchasesComponent {
+  purchases: Purchase[] = [];
+  private purchasesURL: string;
 
+  constructor(private http: HttpClient, private alerts: AlertsService, private authService: AuthService) {
+    this.purchasesURL = `https://localhost:7117/api/Transactions/type/0/user/${this.authService.getUserId()}`;
+  }
+
+  ngOnInit(): void {
+    this.getPurchases();
+  }
+
+  getPurchases(): void {
+    this.alerts.enableLoading("A carregar compras...");
+
+    const jwtToken = localStorage.getItem('token');
+
+    this.http.get<Purchase[]>(this.purchasesURL, {
+      headers: { 'Authorization': `Bearer ${jwtToken}` }
+    }).subscribe({
+      next: (data) => {
+        this.purchases = data;
+        this.alerts.disableLoading();
+      },
+      error: (error) => {
+        console.error("Erro ao obter compras: ", error);
+        this.alerts.disableLoading();
+      }
+    })
+  }
+}
+
+interface Purchase {
+  id: number,
+  project: string,
+  quantity: number,
+  date: string,
+  state: string,
+  checkoutSession: string,
+  paymentMethod: string
 }
