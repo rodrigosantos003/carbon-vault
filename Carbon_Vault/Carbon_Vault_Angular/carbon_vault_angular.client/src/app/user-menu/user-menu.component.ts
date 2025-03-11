42a1aada7b02866347ea1e29e7bb4c0b9e2e7a57
RubenDamaso <97023159+RubenDamaso@users.noreply.github.com>
2025-03-11 07:03:24 +0000

    Update admin dashboard conditional rendering

--- Carbon_Vault/Carbon_Vault_Angular/carbon_vault_angular.client/src/app/user-menu/user-menu.component.ts
+++ Carbon_Vault/Carbon_Vault_Angular/carbon_vault_angular.client/src/app/user-menu/user-menu.component.ts
@@ -19,14 +19,14 @@ export class UserMenuComponent {
   userInitial: string;
 
   adminMenu = [
-    { icon: 'images/menu/DashboardIcon.png', label: 'Dashboard', route: '/admin-dashboard' },
+    { icon: 'images/menu/DashboardIcon.png', label: 'Dashboard', route: '/dashboard' },
     { icon: 'images/menu/UserIconB.png', label: 'Gestão de Utilizadores', route: '/users-manager' },
     { icon: 'images/menu/PurchasesIcon.png', label: 'Gestão de Compras', route: '/admin-dashboard' },
     { icon: 'images/menu/ProjectsIcon.png', label: 'Gestão de Projetos', route: '/project-manager' },
-    { icon: 'images/menu/RelatoriosIcon.png', label: 'Relatórios', route: '/admin-dashboard' },
-    { icon: 'images/menu/TicketsIcon.png', label: 'Tickets', route: '/admin-dashboard' },
-    { icon: 'images/menu/MarketplaceIcon.png', label: 'Marketplace', route: '/admin-dashboard' },
-    { icon: 'images/menu/SettingsIcon.png', label: 'Definições', route: '/admin-dashboard' },
+    { icon: 'images/menu/RelatoriosIcon.png', label: 'Relatórios', route: '/dashboard' },
+    { icon: 'images/menu/TicketsIcon.png', label: 'Tickets', route: '/dashboard' },
+    { icon: 'images/menu/MarketplaceIcon.png', label: 'Marketplace', route: '/dashboard' },
+    { icon: 'images/menu/SettingsIcon.png', label: 'Definições', route: '/dashboard' },
   ];
 
   userMenu = [
