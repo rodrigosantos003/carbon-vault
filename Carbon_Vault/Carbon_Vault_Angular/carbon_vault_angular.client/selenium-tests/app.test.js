9898436f3f8468cdd567aead318d4f5352d3876d
JoaoFernandes02 <joaomlfernandes02@gmail.com>
2025-03-21 18:50:27 +0000

    selenium tests

--- Carbon_Vault/Carbon_Vault_Angular/carbon_vault_angular.client/selenium-tests/app.test.js
+++ Carbon_Vault/Carbon_Vault_Angular/carbon_vault_angular.client/selenium-tests/app.test.js
@@ -2,6 +2,9 @@ import { Builder, By, until } from 'selenium-webdriver';
 import { testLogin } from './tests/login.js';
 import { testEmissionsForm } from './tests/user_emissions.js';
 import { testRegister } from './tests/register.js'
+import { testTransactionsPreview, testTransactionDetailsPreview } from './tests/view_transactions.js';
+import { testCreditsSellCriterion } from './tests/credits-criterion.js';
+import { testAddProject } from './tests/add-project.js';
 
 (async function appTest() {
   let driver = await new Builder().forBrowser('chrome').build();
@@ -14,14 +17,62 @@ import { testRegister } from './tests/register.js'
     // Fazer login e obter o token
     let token = await testLogin(driver, 'user@carbonvault.com', 'User@111');
 
-    // // Definir manualmente o token no localStorage
+    // Definir manualmente o token no localStorage
     await driver.executeScript(`localStorage.setItem('token', '${token}');`);
 
-    // // Recarregar a página para garantir que o token é aplicado
+    // Recarregar a página para garantir que o token é aplicado
     await driver.navigate().refresh();
 
-    // // Testar form de emissões
+    // Testar inserção de projeto
+    await testAddProject(driver);
+
+    // Testar form de emissões
     await testEmissionsForm(driver);
+
+    // Testar critérios de venda de créditos
+    await testCreditsSellCriterion(driver, 2);
+
+    // Testar a visualização de transações de um user com transações
+    await testTransactionsPreview(driver, false, true);
+
+    // Remove o token do localStorage
+    await driver.executeScript(`localStorage.removeItem('token');`);
+
+    // Recarregar a página para garantir que o token é removido
+    await driver.navigate().refresh();
+
+    // Loga com o admin
+    token = await testLogin(driver, 'user2@carbonvault.com', 'User@222');
+
+    // Definir manualmente o token no localStorage
+    await driver.executeScript(`localStorage.setItem('token', '${token}');`);
+
+    // Recarregar a página para garantir que o token é aplicado
+    await driver.navigate().refresh();
+
+    // Testar a visualização de transações de um user
+    await testTransactionsPreview(driver, false, false);
+
+    // Remove o token do localStorage
+    await driver.executeScript(`localStorage.removeItem('token');`);
+
+    // Recarregar a página para garantir que o token é removido
+    await driver.navigate().refresh();
+
+    // Loga com o admin
+    token = await testLogin(driver, 'admin@carbonvault.com', 'Admin@123');
+
+    // Definir manualmente o token no localStorage
+    await driver.executeScript(`localStorage.setItem('token', '${token}');`);
+
+    // Recarregar a página para garantir que o token é aplicado
+    await driver.navigate().refresh();
+
+    // Testar a visualização de transações de um admin
+    await testTransactionsPreview(driver, true, true);
+
+    // Testar a visualização de detalhes de uma transação
+    await testTransactionDetailsPreview(driver);
 
     console.log("Testes concluído com sucesso!");
 
